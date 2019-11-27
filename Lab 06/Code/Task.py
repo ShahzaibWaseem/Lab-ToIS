1d85c611801317a7cb92fb8bf1127ee853d14e55
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2019-11-27 13:01:02 +0500

    Adding Lab 06 - HTML Cleaner - COde

--- Lab 06/Code/Task.py
+++ Lab 06/Code/Task.py
@@ -4,6 +4,6 @@ from urllib.request import urlopen
 
 url = "https://www.w3.org/TR/PNG/iso_8859-1.txt"    
 html = urlopen(url).read()    
-raw = BeautifulSoup(html).get_text()
+raw = BeautifulSoup(html, features="html.parser").get_text()
 tokens = nltk.word_tokenize(raw) 
 print("Tokens: ", tokens)
